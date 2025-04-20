4d2e819c44a88ac26155c9afa2b11dd1ed2904a8
NOTE\paulbir <paulbir@ya.ru>
2025-04-20 23:19:00 +0300

    Started implementing choosing pairs with cointegration and granger causality. Started implementing Spread calculaiton.

---
+++ comovement.py
@@ -0,0 +1,74 @@
+import logging
+from enum import IntEnum
+
+import numpy as np
+import pandas as pd
+from arch.unitroot.cointegration import phillips_ouliaris
+from statsmodels.tsa.stattools import grangercausalitytests
+
+
+class ComovementType(IntEnum):
+	COINTEGRATION = 0
+	GRANGER_CAUSALITY = 1
+
+def test_cointegration(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> tuple[bool, tuple[str, str]]:
+	"""
+	This function checks for cointegration between 2 time series with Phillips-Ouliaris Zt and Za tests
+
+	:param prices_df: Pandas DataFrame with 2 columns (one for each time series).
+	:param combination: Tuple with name of each crypto pair. Should match prices_df column names.
+	:param significance_level: Significance level against which we check our p_value.
+	:return: Tuple of Boolean flag indicating cointegration and combination tuple (for using with parallelization).
+	"""
+
+	po_pvalues = [phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Zt").pvalue,
+				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Za").pvalue,
+				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pu").pvalue,
+				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pz").pvalue]
+	cointegrated = np.all([pvalue < significance_level for pvalue in po_pvalues])
+	if not cointegrated:
+		logging.info(f'{combination} is not cointegrated. po_pvalues: {po_pvalues}')
+		return False, combination
+
+	return True, combination
+
+def test_granger_causality(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> tuple[bool, tuple[str, str]]:
+	"""
+	This function checks if time series 1 first differences are Granger Caused by time series 2 first differences.
+
+	:param prices_df: Pandas DataFrame with 2 columns (one for each time series).
+	:param combination: Tuple with name of each crypto pair. Should match prices_df column names.
+	:param significance_level: Significance level against which we check our p_value.
+	:return: Tuple of Boolean flag indicating Granger Causality and combination tuple (for using with parallelization).
+	"""
+
+	diffs = prices_df.diff(1).dropna()
+	gc_res = grangercausalitytests(diffs, maxlag=10)
+
+	gc_pvalues = [v[1] for v in gc_res.values()]
+	granger_caused = np.any([pvalue < significance_level for pvalue in gc_pvalues])
+
+	if not granger_caused:
+		logging.info(f'{combination[1]} does NOT cause {combination[0]}. gc_pvalues for lags: {gc_pvalues}')
+		return False, combination
+
+	return True, combination
+
+def TestCombinationComovement(prices_df: pd.DataFrame, combination: tuple[str, str], comovement_type: ComovementType, significance_level=0.05) \
+		-> tuple[bool, tuple[str, str]]:
+	"""
+	This function checks if time series 1 first differences are Granger Caused by time series 2 first differences.
+
+	:param prices_df: Pandas DataFrame with 2 columns (one for each time series).
+	:param combination: Tuple with name of each crypto pair. Should match prices_df column names.
+	:param comovement_type: Type of method to choose for testing for comevement.
+	:param significance_level: Significance level against which we check our p_value.
+	:return: Tuple of Boolean flag indicating Granger Causality and combination tuple (for using with parallelization).
+	"""
+
+	if comovement_type == ComovementType.COINTEGRATION:
+		return test_cointegration(prices_df, combination)
+	elif comovement_type == ComovementType.GRANGER_CAUSALITY:
+		return test_granger_causality(prices_df, combination)
+	else:
+		raise(f'Unknown comovement type: {comovement_type}')
---
+++ spread.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import statsmodels.api as sm
+
+def BuildSpread(prices_df: pd.DataFrame, combination: tuple[str, str]) -> tuple[pd.DataFrame, float, tuple[str, str]]:
+	"""
+	This function creates spread dataframe with OLS for train set.
+
+	:param prices_df: Pandas DataFrame with 2 columns (one for each time series).
+	:param combination: Tuple with name of each crypto pair. Should match prices_df column names.
+	:return: Tuple of spread dataframe and combination tuple (for using with parallelization).
+	"""
+
+	Y = prices_df[combination[0]]
+	X = prices_df[combination[1]]
+	X = sm.add_constant(X)
+	model = sm.OLS(Y, X)
+	results = model.fit_regularized()
+	coef = results.params[1]
+
+	spread = pd.DataFrame(Y - X * coef, columns=['spread'], index=prices_df.index)
+
+	return spread, coef, combination
+
+def UpdateSpread(prices_df: pd.DataFrame, combination: tuple[str, str], coef: float) -> tuple[pd.DataFrame, tuple[str, str]]:
+	"""
+	This function creates spread dataframe with coefficient given as parameter.
+
+	:param prices_df: Pandas DataFrame with 2 columns (one for each time series).
+	:param combination: Tuple with name of each crypto pair. Should match prices_df column names.
+	:param coef: spread coefficient.
+	:return: Tuple of spread dataframe and combination tuple (for using with parallelization).
+	"""
+
+	Y = prices_df[combination[0]]
+	X = prices_df[combination[1]]
+	spread = pd.DataFrame(Y - X * coef, columns=['spread'], index=prices_df.index)
+
+	return spread, combination
