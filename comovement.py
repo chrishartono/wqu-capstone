Fixes in spread calculation and feature engineering. Now spread is calculated with cointegration vector. zScore is calculated on window basis.
(cherry picked from commit 9297d692038745e94fd3c0d0ba4193248cde8a6b)

comovement.py
@@ -1,8 +1,10 @@
 import logging
 from enum import IntEnum
+from unittest import result
 
 import numpy as np
 import pandas as pd
+from arch.unitroot._phillips_ouliaris import PhillipsOuliarisTestResults
 from arch.unitroot.cointegration import phillips_ouliaris
 from statsmodels.tsa.stattools import grangercausalitytests
 
@@ -11,7 +13,8 @@ class ComovementType(IntEnum):
 	COINTEGRATION = 0
 	GRANGER_CAUSALITY = 1
 
-def test_cointegration(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> tuple[bool, tuple[str, str]]:
+def test_cointegration(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> \
+		tuple[bool, tuple[str, str], PhillipsOuliarisTestResults]:
 	"""
 	This function checks for cointegration between 2 time series with Phillips-Ouliaris Zt and Za tests
 
@@ -21,21 +24,22 @@ def test_cointegration(prices_df: pd.DataFrame, combination: tuple[str, str], si
 	:return: Tuple of Boolean flag indicating cointegration and combination tuple (for using with parallelization).
 	"""
 	result = phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Zt")
-	v = result.cointegrating_vector
-	spread = prices_df[combination[0]]*v[combination[0]] + prices_df[combination[1]]*v[combination[1]]
+	coint_vector = result.cointegrating_vector
 
-	po_pvalues = [phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Zt").pvalue,
-				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Za").pvalue,
-				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pu").pvalue,
-				  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pz").pvalue]
-	cointegrated = np.all([pvalue < significance_level for pvalue in po_pvalues])
+	# po_pvalues = [phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Zt").pvalue,
+	# 			  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Za").pvalue,
+	# 			  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pu").pvalue,
+	# 			  phillips_ouliaris(prices_df[combination[0]], prices_df[combination[1]], trend="ct", test_type="Pz").pvalue]
+	# cointegrated = np.all([pvalue < significance_level for pvalue in po_pvalues])
+	cointegrated = result.pvalue < significance_level
 	if not cointegrated:
-		logging.info(f'{combination} is not cointegrated. po_pvalues: {po_pvalues}')
-		return False, combination
+		logging.info(f'{combination} is not cointegrated. pvalue: {result.pvalue}')
+		return False, combination, coint_vector
 
-	return True, combination
+	return True, combination, coint_vector
 
-def test_granger_causality(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> tuple[bool, tuple[str, str]]:
+def test_granger_causality(prices_df: pd.DataFrame, combination: tuple[str, str], significance_level=0.05) -> \
+		tuple[bool, tuple[str, str], PhillipsOuliarisTestResults]:
 	"""
 	This function checks if time series 1 first differences are Granger Caused by time series 2 first differences.
 
@@ -45,6 +49,11 @@ def test_granger_causality(prices_df: pd.DataFrame, combination: tuple[str, str]
 	:return: Tuple of Boolean flag indicating Granger Causality and combination tuple (for using with parallelization).
 	"""
 
+	cointegrated, _, coint_vector = test_cointegration(prices_df, combination, significance_level)
+	if not cointegrated:
+		logging.info(f'Will not test Granger causality further for {combination} because not cointegrated.')
+		return False, combination, coint_vector
+
 	diffs = prices_df.diff(1).dropna()
 	gc_res = grangercausalitytests(diffs, maxlag=10)
 
@@ -53,12 +62,12 @@ def test_granger_causality(prices_df: pd.DataFrame, combination: tuple[str, str]
 
 	if not granger_caused:
 		logging.info(f'{combination[1]} does NOT cause {combination[0]}. gc_pvalues for lags: {gc_pvalues}')
-		return False, combination
+		return False, combination, coint_vector
 
-	return True, combination
+	return True, combination, coint_vector
 
-def TestCombinationComovement(prices_df: pd.DataFrame, combination: tuple[str, str], comovement_type: ComovementType, significance_level=0.05) \
-		-> tuple[bool, tuple[str, str]]:
+def TestCombinationComovement(prices_df: pd.DataFrame, combination: tuple[str, str], comovement_type: ComovementType, significance_level=0.05) -> \
+		tuple[bool, tuple[str, str], PhillipsOuliarisTestResults]:
 	"""
 	This function checks if time series 1 first differences are Granger Caused by time series 2 first differences.
 

feature_engineering.py
@@ -23,34 +23,22 @@ def add_basic_features(df: pd.DataFrame, combination: tuple[str, str]):
 
 	return df
 
-def add_zscores(train: pd.DataFrame, test: pd.DataFrame):
-	for col in train.columns:
-		mean = train[col].mean()
-		std = train[col].std()
-		train[f'{col}_zscore'] = (train[col] - mean) / std
-		test[f'{col}_zscore'] = (test[col] - mean) / std
+def add_zscores(data: pd.DataFrame, window_periods: int):
+	for col in data.columns:
+		mean = data[col].rolling(window=window_periods).mean()
+		std = data[col].rolling(window=window_periods).std()
+		data[f'{col}_zscore'] = (data[col] - mean) / std
 
-	return train, test
+	return data
 
-def add_rolling_hurst(train: pd.DataFrame, test: pd.DataFrame, rolling_window_days: int):
+def add_rolling_hurst(data: pd.DataFrame, window_periods: int):
 	hurst_columns = ['spread']
 
-	df = pd.concat([train, test], axis=0)
-	rolling_delta = timedelta(days=rolling_window_days)
-	rolling_window_end_date = df.index[0] + rolling_delta
-	df_slice = df[df.index <= rolling_window_end_date]
-
-	rolling_window_periods = len(df_slice)
 	for col in hurst_columns:
-		df[f'{col}_shifted'] = df[col] + abs(df[col].min()) + 1
-		df[f'{col}_hurst'] = df[f'{col}_shifted'].rolling(window=rolling_window_periods).apply(lambda x: compute_Hc(x, kind='price', simplified=True)[0],
-																							   raw=True)
+		data[f'{col}_shifted'] = data[col] + abs(data[col].min()) + 1
+		data[f'{col}_hurst'] = data[f'{col}_shifted'].rolling(window=window_periods).apply(lambda x: compute_Hc(x, kind='price', simplified=True)[0], raw=True)
 
-	train_len = len(train)
-	train = df.iloc[:train_len]
-	test = df.iloc[train_len:]
-
-	return train, test
+	return data
 
 def clean(df: pd.DataFrame):
 	df.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -59,14 +47,21 @@ def clean(df: pd.DataFrame):
 
 	return df
 
-def AddFeatures(train: pd.DataFrame, test: pd.DataFrame, combination: tuple[str, str], rolling_window_days: int):
-	train = add_basic_features(train, combination)
-	test = add_basic_features(test, combination)
+def AddFeatures(train: pd.DataFrame, test: pd.DataFrame, combination: tuple[str, str], rolling_window_days: int) -> tuple[pd.DataFrame, pd.DataFrame]:
+	data = pd.concat([train, test], axis=0)
 
-	train, test = add_zscores(train, test)
-	train, test = add_rolling_hurst(train, test, rolling_window_days)
+	rolling_delta = timedelta(days=rolling_window_days)
+	rolling_window_end_date = data.index[0] + rolling_delta
+	df_slice = data[data.index <= rolling_window_end_date]
 
-	train = clean(train)
-	test = clean(test)
+	window_periods = len(df_slice)
+
+	data = add_basic_features(data, combination)
+	data = add_zscores(data, window_periods)
+	data = add_rolling_hurst(data, window_periods)
+
+	data = clean(data)
+	train = data.iloc[:len(train)]
+	test = data.iloc[len(train):]
 
 	return train, test