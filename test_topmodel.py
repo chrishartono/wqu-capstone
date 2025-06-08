b33d60b786756325ef5d0b148c2c9651e7c548c6
Boyan Davidov <63375774+davidovg@users.noreply.github.com>
2025-06-08 23:18:06 +0300

    hmm predict tests

---
+++ test_topmodel.py
@@ -0,0 +1,56 @@
+
+#test 
+from hmmlearn import hmm
+from top_model import TopModelHMM,TopModelArima
+from spread import AddPolyfitSpread
+
+#%%
+prices_df = pd.read_parquet('dataset/binance_1h_ohlcv_2021-2025.parquet')
+prices_df = prices_df.set_index("date")
+
+prices_df = prices_df[(prices_df.index >= '2022-01-01') & (prices_df.index <= '2024-09-01')]
+
+#%%
+from combinations import CreateAllPossibleCombinations
+pairs = CreateAllPossibleCombinations(prices_df)
+
+
+# %%
+def AddPolyfitSpread(prices_df, combination, coefs=None, degree=1):
+    df = prices_df.copy()
+    Y = df[combination[0]].astype('float64')
+    X = df[combination[1]].astype('float64')
+    mask = np.isfinite(X) & np.isfinite(Y)
+    X = X[mask]
+    Y = Y[mask]
+    if len(X) < 2:
+        df['spread'] = np.nan
+        return df, None
+    if coefs is None:
+        coefs = np.polyfit(X, Y, degree)
+    Y_fit = np.polyval(coefs, X)
+    spread = Y - Y_fit
+    df['spread'] = np.nan
+    df.loc[spread.index, 'spread'] = spread
+    return df, coefs
+
+# %%
+combination = ('close_btc-usdt', 'close_eth-usdt')
+spread_df, coefs = AddPolyfitSpread(prices_df, combination, coefs=None)
+print(spread_df[['spread', combination[0], combination[1]]].head(10))
+
+# %%
+train_frac = 0.8
+train_idx = int(train_frac * len(spread_df))
+pre_data = spread_df.iloc[:train_idx].copy()
+test_data = spread_df.iloc[train_idx:].copy()
+
+# %%
+top_hmm = TopModelHMM(pre_data, window=24, reference_column='spread')
+preds = top_hmm.predict(test_data)
+print(preds[:10])
+
+# %%
+top_arima = TopModelArima(pre_data, window=24, reference_column='spread')
+arima_preds = top_arima.predict(test_data)
+print(arima_preds[:10])
--- top_model.py
+++ top_model.py
@@ -1,8 +1,10 @@
+#%%
 import numpy as np
 import pandas as pd
 from enum import IntEnum
 from statsmodels.tsa.arima.model import ARIMA
 from tqdm import tqdm
+from hmmlearn import hmm
 
 class TopModelType(IntEnum):
 	HMM = 0
@@ -53,5 +55,110 @@ class TopModelArima:
 
 		return pred
 	
+#%%
+
+
 class TopModelHMM:
-	pass
+    def __init__(self, pre_data: pd.DataFrame, 
+                 window: int = 24, reference_column: str = 'spread', 
+                 n_components: int = 2):
+        self.__window = window
+        self.__pre_data = pre_data
+        self.__reference_column = reference_column
+        self.__n_components = n_components
+
+        # Fit initial HMM on pre_data
+        self.model = self.fit_hmm(pre_data[reference_column].values.reshape(-1, 1))
+
+    def fit_hmm(self, data):
+        # Gaussian HMM, fit to the windowed spread TODO other tests
+        model = hmm.GaussianHMM(n_components=self.__n_components, covariance_type="full", n_iter=100)
+        model.fit(data)
+        return model
+
+    def predict(self, data: pd.DataFrame):
+        pred = []
+        all_data = pd.concat([self.__pre_data, data], axis=0)
+        spread_series = all_data[self.__reference_column].values
+
+        for i in tqdm(range(len(data)), desc='generating HMM regime analysis'):
+            if i < self.__window:
+                window_data = np.concatenate([
+                    self.__pre_data[self.__reference_column].values[-self.__window + i:], 
+                    data[self.__reference_column].values[:i]
+                ]).reshape(-1, 1)
+            else:
+                window_data = data[self.__reference_column].values[i - self.__window:i].reshape(-1, 1)
+
+            # Re-fit HMM
+            hmm_model = self.fit_hmm(window_data)
+            hidden_states = hmm_model.predict(window_data)
+
+            # we need lower variance to allow trading
+            means = hmm_model.means_.flatten()
+            covars = hmm_model.covars_.flatten()
+            low_var_state = np.argmin(covars)
+            
+            current_state = hidden_states[-1]
+            pred.append(1 if current_state == low_var_state else 0)
+
+        return pred
+
+#test 
+from hmmlearn import hmm
+from spread import AddPolyfitSpread
+
+#%%
+prices_df = pd.read_parquet('dataset/binance_1h_ohlcv_2021-2025.parquet')
+prices_df = prices_df.set_index("date")
+
+prices_df = prices_df[(prices_df.index >= '2022-01-01') & (prices_df.index <= '2024-09-01')]
+
+#%%
+from combinations import CreateAllPossibleCombinations
+pairs = CreateAllPossibleCombinations(prices_df)
+
+
+
+
+# %%
+def AddPolyfitSpread(prices_df, combination, coefs=None, degree=1):
+    df = prices_df.copy()
+    Y = df[combination[0]].astype('float64')
+    X = df[combination[1]].astype('float64')
+    mask = np.isfinite(X) & np.isfinite(Y)
+    X = X[mask]
+    Y = Y[mask]
+    if len(X) < 2:
+        df['spread'] = np.nan
+        return df, None
+    if coefs is None:
+        coefs = np.polyfit(X, Y, degree)
+    Y_fit = np.polyval(coefs, X)
+    spread = Y - Y_fit
+    df['spread'] = np.nan
+    df.loc[spread.index, 'spread'] = spread
+    return df, coefs
+
+# %%
+combination = ('close_btc-usdt', 'close_eth-usdt')
+spread_df, coefs = AddPolyfitSpread(prices_df, combination, coefs=None)
+print(spread_df[['spread', combination[0], combination[1]]].head(10))
+
+# %%
+train_frac = 0.8
+train_idx = int(train_frac * len(spread_df))
+pre_data = spread_df.iloc[:train_idx].copy()
+test_data = spread_df.iloc[train_idx:].copy()
+
+# %%
+top_hmm = TopModelHMM(pre_data, window=24, reference_column='spread')
+preds = top_hmm.predict(test_data)
+print(preds[:10])
+
+# %%
+top_arima = TopModelArima(pre_data, window=24, reference_column='spread')
+arima_preds = top_arima.predict(test_data)
+print(arima_preds[:10])
+
+# %%
