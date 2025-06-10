Fixed position calculation in equity calculations.

backtester.py
@@ -230,8 +230,8 @@ class Backtester:
 		pair_pos[0].append(last_pair_pos[0])
 		pair_pos[1].append(last_pair_pos[1])
 		# Sign is already in last_pair_pos, so coef is taken as abs
-		pair_mtm[0].append(last_pair_cash_pos[0] + last_pair_pos[0] * prices[0][i] * abs(coef[0]))
-		pair_mtm[1].append(last_pair_cash_pos[1] + last_pair_pos[1] * prices[1][i] * abs(coef[1]))
+		pair_mtm[0].append(last_pair_cash_pos[0] + last_pair_pos[0] * prices[0][i])
+		pair_mtm[1].append(last_pair_cash_pos[1] + last_pair_pos[1] * prices[1][i])
 
 	def __finalize_backtest(self,
 							prices,
@@ -343,7 +343,7 @@ class Backtester:
 					# And as this is a cumulative cash_pos, we add it to the previous one.
 					last_pair_cash_pos = [last_pair_cash_pos[0] - prices[0][i] * coef[0], last_pair_cash_pos[1] - prices[1][i] * coef[1]]
 					# Positions counted as number of trades. Add to the previous.
-					last_pair_pos = [last_pair_pos[0] + np.sign(coef[0]), last_pair_pos[1] + np.sign(coef[1])]
+					last_pair_pos = [last_pair_pos[0] + coef[0], last_pair_pos[1] + coef[1]]
 					last_fees = [abs(prices[0][i] * coef[0] * self.__fees), abs(prices[1][i] * coef[1] * self.__fees)]
 
 			elif prediction == SignalTypes.SELL.value:
@@ -351,13 +351,12 @@ class Backtester:
 					combination_exposure_trades -= 1
 					# Opposite here. Flip the signs.
 					last_pair_cash_pos = [last_pair_cash_pos[0] + prices[0][i] * coef[0], last_pair_cash_pos[1] + prices[1][i] * coef[1]]
-					last_pair_pos = [last_pair_pos[0] - np.sign(coef[0]), last_pair_pos[1] - np.sign(coef[1])]
+					last_pair_pos = [last_pair_pos[0] - coef[0], last_pair_pos[1] - coef[1]]
 					last_fees = [abs(prices[0][i] * coef[0] * self.__fees), abs(prices[1][i] * coef[1] * self.__fees)]
 
 			elif self.__close_on_no_signal and combination_exposure_trades != 0: # No signal, close position if it is open
-				last_pair_cash_pos = [last_pair_cash_pos[0] + prices[0][i] * last_pair_pos[0] * abs(coef[0]),
-									  last_pair_cash_pos[1] + prices[1][i] * last_pair_pos[1] * abs(coef[1])]
-				last_fees = [abs(prices[0][i] * last_pair_pos[0] * coef[0] * self.__fees), abs(prices[1][i] * last_pair_pos[1] * coef[1] * self.__fees)]
+				last_pair_cash_pos = [last_pair_cash_pos[0] + prices[0][i] * last_pair_pos[0], last_pair_cash_pos[1] + prices[1][i] * last_pair_pos[1]]
+				last_fees = [abs(prices[0][i] * last_pair_pos[0] * self.__fees), abs(prices[1][i] * last_pair_pos[1] * self.__fees)]
 				last_pair_pos = [0, 0]
 				combination_exposure_trades = 0
 

test.py
@@ -48,7 +48,7 @@ def backtest_test(prices_df: pd.DataFrame, num_good_combs_to_choose: int, min_va
 
 	all_possible_combinations = CreateAllPossibleCombinations(prices_df)
 	# np.random.shuffle(all_possible_combinations)
-	all_possible_combinations_slice = all_possible_combinations[:50]
+	# all_possible_combinations_slice = all_possible_combinations[:50]
 
 	# all_possible_combinations_slice = [('close_vet-usdt', 'close_sc-usdt')]
 
@@ -58,20 +58,22 @@ def backtest_test(prices_df: pd.DataFrame, num_good_combs_to_choose: int, min_va
 	# 								   ('close_rei-usdt', 'close_algo-usdt'), ('close_voxel-usdt', 'close_algo-usdt'), ('close_amp-usdt', 'close_bico-usdt'),
 	# 								   ('close_badger-usdt', 'close_ach-usdt'), ('close_amp-usdt', 'close_celo-usdt'), ('close_rei-usdt', 'close_ach-usdt')]
 	train_window_days = 660
-	val_window_days = 180
-	trade_window_days = 30
+	val_window_days = 60
+	trade_window_days = 60
+	spread_window = 5
+	target_window = 20
 	# train_window_days = (prices_df.index[-1] - prices_df.index[0]).days - trade_window_days
 	# target_params = {'numNeighbours': 10, 'rolling_window_days': 10}
-	target_params = {'look_ahead_days': 5, 'reg_points_thresh_frac': 0.75, 'exceedance_thresh_frac': 0.001}
+	target_params = {'look_ahead_days': target_window, 'reg_points_thresh_frac': 0.75, 'exceedance_thresh_frac': 0.001}
 	backtester = Backtester(prices_df=prices_df,
 							train_window_days=train_window_days,
 							ml_val_window_days=trade_window_days,
 							trade_window_days=trade_window_days,
 							val_window_days=val_window_days,
 							features_rolling_windows_days_list=[1, 5, 10],
-							all_possible_combinations=all_possible_combinations_slice,
+							all_possible_combinations=all_possible_combinations,
 							comovement_detection_type=ComovementType.GC_MI,
-							use_parallelization=False,
+							use_parallelization=True,
 							combination_limit=1000,
 							trade_limit=1000,
 							risk_free_rate=0,
@@ -85,7 +87,8 @@ def backtest_test(prices_df: pd.DataFrame, num_good_combs_to_choose: int, min_va
 							close_on_no_signal=False,
 							use_jump_features=use_jump_features,
 							use_copula_features=use_copula_features,
-							reference_prices_column='close_btc-usdt')
+							reference_prices_column='close_btc-usdt',
+							spread_window=spread_window)
 	backtester.Run()
 
 def run_consecutive_backtests():
@@ -209,16 +212,16 @@ if __name__ == '__main__':
 	os.makedirs('logs', exist_ok=True)
 	# parallel_logging(f'logs/wqu_capstone_{now_str}.log')
 
-	run_consecutive_ml_quality_tests()
+	# run_consecutive_ml_quality_tests()
 	# TODO: Test run
 	# prices_df = prices_df[(prices_df.index >= '2023-02-01') & (prices_df.index <= '2024-07-01')]
 	# prices_df = prices_df[(prices_df.index >= '2022-01-01') & (prices_df.index <= '2024-09-01')]
 
 	# manual_test(prices_df)
-	# log_file_name = (f'logs/backtest_test.log')
-	# SetLogging(log_file_name)
-	# prices_df = pd.read_csv('dataset/binance_1h_ohlcv_2021-2025.csv', index_col='date', parse_dates=True)
-	# prices_df = prices_df[(prices_df.index >= '2022-01-01') & (prices_df.index <= '2025-03-01')]
-	# backtest_test(prices_df, num_good_combs_to_choose=1, min_val_net_return=0.3, min_val_num_trades=60, use_jump_features=True, use_copula_features=False)
-	# logging.info('Finished')
+	log_file_name = (f'logs/backtest_test_many_trades.log')
+	SetLogging(log_file_name)
+	prices_df = pd.read_csv('dataset/binance_1h_ohlcv_2021-2025.csv', index_col='date', parse_dates=True)
+	prices_df = prices_df[(prices_df.index >= '2022-01-01') & (prices_df.index <= '2024-09-01')]
+	backtest_test(prices_df, num_good_combs_to_choose=200, min_val_net_return=0.1, min_val_num_trades=120, use_jump_features=False, use_copula_features=False)
+	logging.info('Finished')
 	# run_consecutive_backtests()